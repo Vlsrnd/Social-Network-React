740942ebba1528c4b149a4656f9412e0a421564e
Leonid Velichko <vlsrnd@gmail.com>
2020-11-24 10:25:12 +0300

    add components Header, Navbar, Profile

--- src/App.js
+++ src/App.js
@@ -1,51 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img src="someimg" alt="logo"></img>
-      </header>
-      <nav className="nav">
-        <div>
-          <a>Profile</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Setting</a>
-        </div>
-      </nav>
-      <div className="content">
-        <div>
-          <img src="som" alt="big picture"></img>
-        </div>
-        <div>
-          <img srs="sfdf" alt="avatar"></img>
-          ava + description
-        </div>
-        <div>
-          My posts
-          <div>
-            New Post
-          </div>
-          <div>
-            post1
-          </div>
-          <div>
-            post2
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+  return (      
+    <div className="content">
+      <div>
+        <img src="./../" alt="big picture"></img>
+      </div>
+      <div>
+        <img srs="sfdf" alt="avatar"></img>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New Post
+        </div>
+        <div>
+          post1
+        </div>
+        <div>
+          post2
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
