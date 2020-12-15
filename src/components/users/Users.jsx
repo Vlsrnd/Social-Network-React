f842f78100c22d735b1ba53ab63159179dc5bb57
Leonid Velichko <vlsrnd@gmail.com>
2020-12-15 17:38:51 +0300

    add axios

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,39 +1,18 @@
 import React from 'react';
-import styles from './users.module.css'
+import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
   
   if (props.users.length === 0){
-    props.setUsers([ 
-      { id: 1, 
-        name: 'Dmitry', 
-        surname: 'Karapet', 
-        photoUrl: '/ava/ava.jpg', 
-        location: {country: 'Belarus', city: 'Minsk'}, 
-        status: `I'm looking for a Job right now...`, 
-        followed: true},
-      { id: 2, 
-        name: 'Svetlana', 
-        surname: 'Dubina', 
-        photoUrl: '/ava/ava.jpg', 
-        location: {country: 'Belarus', city: 'Minsk'}, 
-        status: `I'm so pretty`, 
-        followed: true},
-      { id: 3, 
-        name: 'Sergey', 
-        surname: 'Sinvil', 
-        photoUrl: '/ava/ava.jpg', 
-        location: {country: 'Ukraine', city: 'Kiev'}, 
-        status: `Yupeeee`, 
-        followed: false},
-      { id: 4, 
-        name: 'Andrew', 
-        surname: 'Tishkin', 
-        photoUrl: '/ava/ava.jpg', 
-        location: {country: 'Russia', city: 'Moscow'}, 
-        status: `mad`, 
-        followed: false}
-    ])
+
+    axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
+          props.setUsers(response.data.items)
+        }
+    );
+
   }
 
   return <div>
@@ -41,7 +20,7 @@ let Users = (props) => {
       props.users.map(user => <div key={user.id}>
         <span>
           <div>
-            <img src={user.photoUrl} alt="ava" className={styles.userPhoto} />
+            <img src={user.photos.small || userPhoto} alt="ava" className={styles.userPhoto} />
           </div>
           <div>
             {user.followed 
@@ -51,12 +30,12 @@ let Users = (props) => {
         </span>
         <span>
           <span>
-            <div className={styles.name}>{user.name} {user.surname}</div>
+            <div className={styles.name}>{user.name}</div>
             <div>{user.status}</div>
           </span>
           <span>
-            <div>{user.location.country}</div>
-            <div>{user.location.city}</div>
+            <div>{"user.location.country"}</div>
+            <div>{"user.location.city"}</div>
           </span>
         </span>
       </div>)
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -9,20 +9,7 @@ const setUsersAC = (users) => ({type: SET_USERS, users});
 
 
 let initialState = {
-  users: [
-          // {id: 1, name: 'Dmitry', surname: 'Karapet', photoUrl: '/ava/ava.jpg', 
-          //   location: {country: 'Belarus', city: 'Minsk'}, status: `I'm looking for a Job right now...`, 
-          //   followed: true},
-          // {id: 2, name: 'Svetlana', surname: 'Dubina', photoUrl: '/ava/ava.jpg', 
-          //   location: {country: 'Belarus', city: 'Minsk'}, status: `I'm so pretty`, 
-          //   followed: true},
-          // {id: 3, name: 'Sergey', surname: 'Sinvil', photoUrl: '/ava/ava.jpg', 
-          //   location: {country: 'Ukraine', city: 'Kiev'}, status: `Yupeeee`, 
-          //   followed: false},
-          // {id: 4, name: 'Andrew', surname: 'Tishkin', photoUrl: '/ava/ava.jpg', 
-          //   location: {country: 'Russia', city: 'Moscow'}, status: `mad`, 
-          //   followed: false},
-          ]
+  users: []
 };
 
 const usersReducer = (state = initialState, action) => {
