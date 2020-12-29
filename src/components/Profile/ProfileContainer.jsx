eee149d65cea02f886dcc273412c0a220f4de048
Leonid Velichko <vlsrnd@gmail.com>
2020-12-29 11:12:32 +0300

    add hoc withRouter into ProfileContainer

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,12 +1,16 @@
 import * as axios from 'axios';
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { setUserProfile } from '../../redux/profileReducer';
 import Profile from './Profile';
 
 class ProfileContainer extends React.Component {
   componentDidMount = () => {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${2}`)
+    let userId = this.props.match.params.userId;
+    debugger
+    if (!userId) userId = 2;
+    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
     .then(response => {
       this.props.setUserProfile(response.data);
     })
@@ -19,4 +23,4 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
+export default connect(mapStateToProps, {setUserProfile})(withRouter(ProfileContainer));
