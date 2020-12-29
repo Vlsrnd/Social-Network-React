da23cf5481a0b8b12b99768a681d5d0c21ba68ca
Leonid Velichko <vlsrnd@gmail.com>
2020-12-29 15:20:02 +0300

    add login functional

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
   return (      
   <header className={styles.header}>
       {/* <img src="./logo192.png" alt="logo"></img> */}
+      <div className={styles.loginBlock}>
+      {
+        props.isAuth ? 'yourName' : <NavLink to={'/login'}>Login</NavLink>
+      }
+      </div>
   </header>
   )
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import { profileReducer } from './profileReducer';
 import { dialogsReducer } from './dialogsReducer';
 import { sidebarReducer } from './sidebarReducer';
 import { usersReducer } from './usersReducer';
+import { authReducer } from './auth-reducer';
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
   usersPage: usersReducer,
+  auth: authReducer,
 });
 
 export const store = createStore(reducers);
