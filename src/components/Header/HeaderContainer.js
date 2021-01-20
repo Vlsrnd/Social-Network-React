37bc17f4bb768b74ca94265eac8cab190d074327
Leonid Velichko <vlsrnd@gmail.com>
2021-01-20 11:44:20 +0300

    refactor: change App to class component. move getAuthUserData from HeaderContainer to App. add compose(withRouter, connect)

--- src/components/Header/HeaderContainer.js
+++ src/components/Header/HeaderContainer.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Header from "./Header";
-import { getAuthUserData, logout } from '../../redux/auth-reducer';
+import { logout } from '../../redux/auth-reducer';
 
 class HeaderContainer extends React.Component {
-  componentDidMount = () => this.props.getAuthUserData();
-
   render = () => <Header {...this.props} />
 }
 
@@ -14,4 +12,4 @@ const mapStateToProps = state => ({
   userName: state.auth.login,
 });
 
-export default connect(mapStateToProps, { getAuthUserData, logout })(HeaderContainer);
+export default connect(mapStateToProps, { logout })(HeaderContainer);
