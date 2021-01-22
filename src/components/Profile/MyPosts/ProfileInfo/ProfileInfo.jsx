05a75cfd2601497b802d7c89abafd279780d0eef
Leonid Velichko <vlsrnd@gmail.com>
2021-01-22 16:43:43 +0300

    refactor(ProfileInfo): destructuring asignment

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -3,16 +3,16 @@ import Preloader from '../../../Preloader/preloader';
 import styles from './ProfileInfo.module.css';
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
-const ProfileInfo = (props) => {
-  if (!props.profile) return <Preloader />
+const ProfileInfo = ({profile, status, updateUserStatus}) => {
+  if (!profile) return <Preloader />
   return (      
     <div>
       <div className={styles.wallpapper}>
         {/* <img src={window.location.origin + '/image/big021.png'} alt="wallpapper"></img> */}
       </div>
       <div className={styles.descriptionBlock}>
-        <img src={props.profile.photos.large} alt="main avatar" />
-        <ProfileStatusWithHooks status={props.status} updateUserStatus={props.updateUserStatus}/>
+        <img src={profile.photos.large} alt="main avatar" />
+        <ProfileStatusWithHooks status={status} updateUserStatus={updateUserStatus}/>
         <div>
           <p>Name: Rick and Morty</p>
           <p>Age: no informations</p>
