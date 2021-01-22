b0409d1a9c3bbac894a94b5cca0e08315c1553e7
Leonid Velichko <vlsrnd@gmail.com>
2021-01-22 15:49:08 +0300

    refactor(FormsControls): destructuring asignment for  meta:{touched, error}

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Field } from 'redux-form';
 import styles from './FormsControls.module.css';
 
-export const FormControl = (element) => ({input, meta, ...props}) => {
-  const hasError = meta.touched && meta.error;
+export const FormControl = (element) => ({input, meta: {touched, error}, ...props}) => {
+  const hasError = touched && error;
   return (
     <div className={styles.formControl + ' ' + (hasError ? styles.error : '')} >
       <div>
         {React.createElement(element, {...input, ...props})}
       </div>
-      { hasError && <span>{meta.error}</span> }
+      { hasError && <span>{error}</span> }
     </div>
   )
 };
