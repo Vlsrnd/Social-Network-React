cdb5fafa32dd3bcfc3c44b878393bdfd535b599c
Leonid Velichko <vlsrnd@gmail.com>
2021-01-22 16:37:46 +0300

    refactor(Users): create User

---
+++ src/components/users/User/User.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import userPhoto from '../../../assets/images/user.png';
+import styles from '../users.module.css';
+
+const User = ({user, followingInProgress, follow, unfollow}) => {
+  return (
+    <div>
+          <span>
+            <div>
+              <NavLink to={'/profile/' + user.id}>
+                <img src={user.photos.small || userPhoto} alt="ava" 
+                  className={styles.userPhoto} />
+              </NavLink>
+            </div>
+            <div>
+              {user.followed 
+                ? <button disabled={followingInProgress.includes(user.id)} 
+                  onClick={() => { unfollow(user.id) }}>
+                  Unfollow</button> 
+                : <button disabled={followingInProgress.includes(user.id)}
+                  onClick={() => { follow(user.id) }}>
+                  Follow</button>}
+            </div>
+          </span>
+          <span>
+            <span>
+              <div className={styles.name}>{user.name}</div>
+              <div>{user.status}</div>
+            </span>
+            <span>
+              <div>{"user.location.country"}</div>
+              <div>{"user.location.city"}</div>
+            </span>
+          </span>
+    </div>
+  )
+}
+
+export default User;
