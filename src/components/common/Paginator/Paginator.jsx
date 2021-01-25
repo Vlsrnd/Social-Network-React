6d974baa6fee44c42182c177fcbc26c2e048ed42
Leonid Velichko <vlsrnd@gmail.com>
2021-01-25 22:45:27 +0300

    feat(Paginator): now paginator hac buttons prev, next and show only limit pages in one time

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Paginator.module.css';
 
-const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged}) => {
-  let pagesCount = Math.ceil(totalUsersCount / pageSize);
+const Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
+  let pagesCount = Math.ceil(totalItemsCount / pageSize);
   let pages = [];
   for (let i = 1; i <= pagesCount; i++){
     pages.push(i);
   };
+
+  let portionCount = Math.ceil(pagesCount / portionSize);
+  let [portionNumber, setPortionNumber] = useState(1);
+  let leftSidePortionNumber = (portionNumber - 1) * portionSize + 1;
+  let rightSidePortionNumber = portionNumber * portionSize;
+
   return (
-    <div>
+    <div className={styles.paginator}>
+      <button onClick={() => setPortionNumber(portionNumber - 1)} disabled={portionNumber <= 1}>prev</button>
       {
-        pages.map(p => {
-          return <span key={p} className={currentPage === p && styles.selectedPage} 
-            onClick={() => { onPageChanged(p) }}>{p}</span>
-          }
+        pages
+          .filter(p => p >= leftSidePortionNumber && p <= rightSidePortionNumber)
+          .map(p => {
+            return <span key={p} className={currentPage === p && styles.selectedPage} 
+              onClick={() => { onPageChanged(p) }}>{p}</span>
+            }
         )
       }
+      <button onClick={() => setPortionNumber(portionNumber + 1)} disabled={portionNumber >= portionCount}>next</button>
     </div>
   )
 }
