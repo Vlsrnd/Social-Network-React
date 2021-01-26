79d18fad5dc5fd3023b3b39c5279b593ac69889f
Leonid Velichko <vlsrnd@gmail.com>
2021-01-26 21:10:45 +0300

    feat(Login): add showing captcha image and input for this

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,12 +8,14 @@ import { createField, FormControl } from '../common/FormsControls/FormsControls'
 import styles from '../common/FormsControls/FormsControls.module.css';
 
 const Input = FormControl('input');
-const LoginForm = ({handleSubmit, error}) => {
+const LoginForm = ({handleSubmit, error, captchaUrl}) => {
   return (
     <form onSubmit={handleSubmit}>
       {createField('Email', 'email', [required], Input)}
       {createField('Password', 'password', [required], Input, {type: 'password'})}
       {createField(null, 'rememberMe', [], Input, {type: 'checkbox'}, 'remember me')}
+      { captchaUrl && <img src={captchaUrl} alt='captcha' /> }
+      { captchaUrl && createField('Captcha symbols', 'captcha', [required], Input) }
       {
       error &&
       <div className={styles.formSummaryError}>
@@ -31,7 +33,7 @@ const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm);
 
 const Login = (props) => {
   const onSubmit = (formData) => {
-    props.login(formData.email, formData.password, formData.rememberMe, true);
+    props.login(formData.email, formData.password, formData.rememberMe, formData.captcha);
   };
 
   if (props.isAuth) return <Redirect to='/profile' />
@@ -39,11 +41,11 @@ const Login = (props) => {
   return (
     <div>
       <h1>Login</h1>
-      <LoginReduxForm onSubmit={onSubmit}/>
+      <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl} />
     </div>
   )
 };
 
-const mapStateToProps = (state) => ({isAuth: state.auth.isAuth});
+const mapStateToProps = (state) => ({captchaUrl: state.auth.captchaUrl, isAuth: state.auth.isAuth});
 
 export default connect(mapStateToProps, { login })(Login);
