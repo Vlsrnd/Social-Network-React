6dd767b189bda867bd549216c4d935bbed6e4e39
Leonid Velichko <vlsrnd@gmail.com>
2021-01-26 09:01:02 +0300

    feat(Profile): add passing savePhoto to ProfileInfo

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,12 @@ import ProfileInfo from './MyPosts/ProfileInfo/ProfileInfo';
 const Profile = (props) => {
   return (      
     <div>
-      <ProfileInfo profile={props.profile} status={props.status} updateUserStatus={props.updateUserStatus}/>
+      <ProfileInfo profile={props.profile} 
+                   status={props.status} 
+                   updateUserStatus={props.updateUserStatus}
+                   isMyProfile={props.isMyProfile}
+                   savePhoto={props.savePhoto}
+                   />
       <MyPostsContainer />
     </div>
   )
