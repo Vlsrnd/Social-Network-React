9e812696481750c9d0b7597011a9c6c4720a46b2
Leonid Velichko <vlsrnd@gmail.com>
2021-01-26 08:00:16 +0300

    feat(ProfileInfo): add standart avatar if no user avatar recieved

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Preloader from '../../../Preloader/preloader';
 import styles from './ProfileInfo.module.css';
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
+import userPhoto from '../../../../assets/images/user.png';
 
 const ProfileInfo = ({profile, status, updateUserStatus}) => {
   if (!profile) return <Preloader />
@@ -11,7 +12,7 @@ const ProfileInfo = ({profile, status, updateUserStatus}) => {
         {/* <img src={window.location.origin + '/image/big021.png'} alt="wallpapper"></img> */}
       </div>
       <div className={styles.descriptionBlock}>
-        <img src={profile.photos.large} alt="main avatar" />
+        <img src={profile.photos.large || userPhoto} className={styles.mainPhoto} alt="main avatar" />
         <ProfileStatusWithHooks status={status} updateUserStatus={updateUserStatus}/>
         <div>
           <p>Name: Rick and Morty</p>
