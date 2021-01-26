ccc06661ce8c7bf8b3783108839a25938136317c
Leonid Velichko <vlsrnd@gmail.com>
2021-01-26 21:12:17 +0300

    feat(auth-reducer): add getCaptchaURL, action type, action creator, dispatch for captcha. add getCaptchaUrl thunk in login thunk

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,19 +1,23 @@
-import { authAPI } from "../api/api";
+import { authAPI, securityAPI } from "../api/api";
 import { stopSubmit } from 'redux-form';
 
 const SET_USER_DATA = 'SET-USER-DATA';
+const GET_CAPTCHA_URL_SUCCESS = 'GET_CAPTCHA_URL_SUCCESS';
 
 let initialState = {
   id: null,
   email: null,
   login: null,
   isAuth: false,
+  captchaUrl: null,
 };
 
 //action creator
 export const setAuthUserData = (id, email, login, isAuth) => {
   return {type: SET_USER_DATA, payload: {id, email, login, isAuth}};
-}
+};
+export const getCaptchaUrlSuccess = (captchaUrl) => ({type: GET_CAPTCHA_URL_SUCCESS, payload: {captchaUrl}});
+
 //thunk
 export const getAuthUserData = () => async (dispatch) => {
   const data = await authAPI.amIAuthorized();
@@ -23,10 +27,17 @@ export const getAuthUserData = () => async (dispatch) => {
   }
 };
 
-export const login = (email, password, rememberMe) => async (dispatch) => {
-  const data = await authAPI.login(email, password, rememberMe)
+export const login = (email, password, rememberMe, captcha) => async (dispatch) => {
+  const data = await authAPI.login(email, password, rememberMe, captcha)
   if (data.resultCode === 0) dispatch(getAuthUserData());
-  else dispatch(stopSubmit('login', {_error: data.messages[0] || 'some error'}));
+  else {
+    if (data.resultCode === 10) dispatch(getCaptchaUrl());
+    dispatch(stopSubmit('login', {_error: data.messages[0] || 'some error'}));
+  }
+};
+export const getCaptchaUrl = () => async (dispatch) => {
+  const data = await securityAPI.getCaptchaUrl();
+  dispatch(getCaptchaUrlSuccess(data.url));
 };
 export const logout = () => async (dispatch) => {
   const data = await authAPI.logout();
@@ -34,9 +45,11 @@ export const logout = () => async (dispatch) => {
   else console.error(data.messages);
 };
 
+
 export const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_USER_DATA:
+    case GET_CAPTCHA_URL_SUCCESS:
       return {
         ...state,
         ...action.payload,
