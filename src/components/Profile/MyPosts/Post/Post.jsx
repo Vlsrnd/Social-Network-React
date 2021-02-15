2554c79dd463cb81a9131d166fd6dc6d780dc2f1
Leonid Velichko <vlsrnd@gmail.com>
2021-02-15 17:33:10 +0300

    refactor(Post): change the structure, add style

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,9 +4,8 @@ import styles from './Post.module.css';
 const Post = (props) => {
   return (      
     <div className={styles.item}>
-      <img src={window.location.origin + '/image/' + props.imgLink} alt="avatar"></img>
       <span>{props.message}</span>
-      <div className={styles.like}>Like ({props.likesCount})</div>
+      <div>Like ({props.likesCount})</div>
     </div>
   )
 }
