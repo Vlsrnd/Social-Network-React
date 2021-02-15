99e2236e5c6aeb7a4e24e64bd62e80ebfa1ce469
Leonid Velichko <vlsrnd@gmail.com>
2021-02-15 14:28:24 +0300

    refactor(User.jsx): remove user location and style fix

--- src/components/users/User/User.jsx
+++ src/components/users/User/User.jsx
@@ -1,40 +1,48 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import userPhoto from '../../../assets/images/user-default.svg';
-import styles from '../users.module.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import userPhoto from "../../../assets/images/user-default.svg";
+import styles from "../users.module.css";
 
-const User = ({user, followingInProgress, follow, unfollow}) => {
+const User = ({ user, followingInProgress, follow, unfollow }) => {
+  debugger
   return (
     <div>
-          <span>
-            <div>
-              <NavLink to={'/profile/' + user.id}>
-                <img src={user.photos.small || userPhoto} alt="ava" 
-                  className={styles.userPhoto} />
-              </NavLink>
-            </div>
-            <div>
-              {user.followed 
-                ? <button disabled={followingInProgress.includes(user.id)} 
-                  onClick={() => { unfollow(user.id) }}>
-                  Unfollow</button> 
-                : <button disabled={followingInProgress.includes(user.id)}
-                  onClick={() => { follow(user.id) }}>
-                  Follow</button>}
-            </div>
-          </span>
-          <span>
-            <span>
-              <div className={styles.name}>{user.name}</div>
-              <div>{user.status}</div>
-            </span>
-            <span>
-              <div>{"user.location.country"}</div>
-              <div>{"user.location.city"}</div>
-            </span>
-          </span>
+      <span>
+        <div>
+          <NavLink to={"/profile/" + user.id}>
+            <img
+              src={user.photos.small || userPhoto}
+              alt="ava"
+              className={styles.userPhoto}
+            />
+          </NavLink>
+        </div>
+        <div>
+          {user.followed ? (
+            <button
+              disabled={followingInProgress.includes(user.id)}
+              onClick={() => unfollow(user.id)}
+            >
+              Unfollow
+            </button>
+          ) : (
+            <button
+              disabled={followingInProgress.includes(user.id)}
+              onClick={() => follow(user.id)}
+            >
+              Follow
+            </button>
+          )}
+        </div>
+      </span>
+      <span>
+        <span>
+          <div className={styles.name}>{user.name}</div>
+          <div>{user.status}</div>
+        </span>
+      </span>
     </div>
-  )
-}
+  );
+};
 
 export default User;
