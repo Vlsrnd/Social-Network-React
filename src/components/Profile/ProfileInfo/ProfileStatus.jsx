4c782600a1e937b207c5709bf29a56120b246d31
Leonid Velichko <vlsrnd@gmail.com>
2021-02-15 16:47:13 +0300

    feat(ProfileStatus): add style for status

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import styles from '../ProfileInfo/ProfileInfo.module.css';
 
 const ProfileStatus = (props) => {
   const [editMode, setEditMode] = useState(false);
   const [status, setStatus] = useState(props.status);
 
-  useEffect( () => {
+  useEffect(() => {
     setStatus(props.status);
   }, [props.status]);
 
@@ -15,9 +16,22 @@ const ProfileStatus = (props) => {
   };
   const onStatusChange = (e) => setStatus(e.currentTarget.value);
 
-  return editMode ? 
-    <input onBlur={() => deactivateEditMode()} onChange={onStatusChange} autoFocus={true} value={status} />
-  : <span onDoubleClick={() => activateEditMode()}>{props.status || '---'}</span>;
+  return (
+    <div className={styles.status}>
+      {editMode ? (
+        <input
+          onBlur={() => deactivateEditMode()}
+          onChange={onStatusChange}
+          autoFocus={true}
+          value={status}
+        />
+      ) : (
+        <span onDoubleClick={() => activateEditMode()}>
+          Status: {props.status || "---"}
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default ProfileStatus;
